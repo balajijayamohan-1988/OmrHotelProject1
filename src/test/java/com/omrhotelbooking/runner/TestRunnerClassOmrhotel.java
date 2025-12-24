576f2e035915a6c167fbc681b834e17047cff715
balajijayamohan-1988 <balaji.jayamohan@gmail.com>
2025-12-24 17:31:36 +0530

    created by tester2

--- src/test/java/com/omrhotelbooking/runner/TestRunnerClassOmrhotel.java
+++ src/test/java/com/omrhotelbooking/runner/TestRunnerClassOmrhotel.java
@@ -5,13 +5,14 @@ import com.omrhotelbooking.stepdfnfile.LoginPageStepDfnFile;
 public class TestRunnerClassOmrhotel extends LoginPageStepDfnFile {
 
 	public static void main(String[] args) {
-		
-		
-		
-		
-		
-		
-		
+
+		System.out.println("SeleniumFrameWrok");
+		System.out.println("FrameWork BaseClass");
+		System.out.println("FrameWork POM");
+		System.out.println("Framework DataDriven");
+		System.out.println("API");
+		System.out.println("MobileTesting");
+
 	}
-			
+
 }
